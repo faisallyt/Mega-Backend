086a207302a6e553ccde8b6f64594365c6070f5f
faisallyt <96649676+faisallyt@users.noreply.github.com>
2024-03-07 17:10:22 +0530

    util code setup and cors and urlencoded learned

--- src/index.js
+++ src/index.js
@@ -9,4 +9,18 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`server is running at port : 
+        ${process.env.PORT}`);
+    })
+
+    app.on("error",(error)=>{
+        console.log("ERR:",error);
+        throw error
+    })
+})
+.catch((error)=>{
+    console.log("MONGO db connection failed !!! ",err);
+})
